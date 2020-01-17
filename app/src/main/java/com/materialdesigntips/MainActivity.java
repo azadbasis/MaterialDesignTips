16f278a33acbbf14fb5cd89c4e34d48a08a4d11c
Azhar <azadbasis@gmail.com>
2020-01-17 09:09:53 +0600

    Raised Button

--- app/src/main/java/com/materialdesigntips/MainActivity.java
+++ app/src/main/java/com/materialdesigntips/MainActivity.java
@@ -1,81 +1,28 @@
 package com.materialdesigntips;
-
 import android.os.Bundle;
-import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatEditText;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.support.v7.widget.AppCompatButton;
 import android.view.View;
-import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    AppCompatEditText user;
-    AppCompatEditText pass;
-    RelativeLayout relativeLayout;
-    TextInputLayout userLayout;
-    TextInputLayout passLayout;
+    AppCompatButton raisedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        raisedButton = (AppCompatButton) findViewById(R.id.raised_btn);
 
-        user = (AppCompatEditText) findViewById(R.id.username_TextField);
-        userLayout = (TextInputLayout) findViewById(R.id.username_TextInputLayout);
-        pass = (AppCompatEditText) findViewById(R.id.password_TextField);
-        passLayout = (TextInputLayout) findViewById(R.id.password_TextInputLayout);
-        relativeLayout = (RelativeLayout) findViewById(R.id.activity_main);
-        relativeLayout.setOnClickListener(null);
-
-        user.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        raisedButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
+            public void onClick(View v) {
 
-                if (user.getText().toString().isEmpty()){
-
-                    userLayout.setErrorEnabled(true);
-                    userLayout.setError("please enter your username!");
-
-                }else {
-
-                    userLayout.setErrorEnabled(false);
-                }
+                Toast.makeText(getApplicationContext(),"Raised Button", Toast.LENGTH_SHORT).show();
             }
         });
-
-        user.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                if (user.getText().toString().isEmpty()){
-
-                    userLayout.setErrorEnabled(true);
-                    userLayout.setError("please enter your username!");
-
-                }else {
-
-                    userLayout.setErrorEnabled(false);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
-        passLayout.setCounterEnabled(true);
-        passLayout.setCounterMaxLength(8);
-
     }
 }
