a12d23523befe95fed78a99f239926b9b9f34cf6
Azhar <azadbasis@gmail.com>
2020-01-25 21:38:57 +0600

    AppCompatRadioButton

--- app/src/main/java/com/materialdesigntips/MainActivity.java
+++ app/src/main/java/com/materialdesigntips/MainActivity.java
@@ -2,36 +2,27 @@ package com.materialdesigntips;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.AppCompatRadioButton;
 import android.widget.CompoundButton;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    AppCompatCheckBox appCompatCheckBox;
+    AppCompatRadioButton radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        appCompatCheckBox = (AppCompatCheckBox) findViewById(R.id.checkbox_id);
+        radioButton = (AppCompatRadioButton) findViewById(R.id.RadioButton1_id);
 
-        appCompatCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
+        radioButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
 
-                if (b){
-
-                    Toast.makeText(getApplicationContext(),"CheckBox is Enable",Toast.LENGTH_LONG).show();
-
-                }else {
-
-                    Toast.makeText(getApplicationContext(),"CheckBox is Disable",Toast.LENGTH_LONG).show();
-
-                }
+                System.out.println(b);
             }
         });
+
     }
 }
